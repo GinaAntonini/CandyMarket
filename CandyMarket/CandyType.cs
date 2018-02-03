ab6604bddb0c2156a8fcd1bd7a5dd2bc65ec7ef9
Gina <ginamantonini@gmail.com>
2018-02-03 09:25:35 -0600

    copied info from e6 CandyMarket repo

---
+++ CandyMarket/CandyType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyMarket
+{
+    enum CandyType
+    {
+        TaffyNotLaffy = 1,
+        CandyCoated,
+        CompressedSugar,
+        ZagnutStyle
+    }
+}
---
+++ CandyMarket/DatabaseContext.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyMarket
+{
+    internal class DatabaseContext
+    {
+        private int _countOfTaffy;
+        private int _countOfCandyCoated;
+        private int _countOfChocolateBar;
+        private int _countOfZagnut;
+
+        /**
+		 * this is just an example.
+		 * feel free to modify the definition of this collection "BagOfCandy" if you choose to implement the more difficult data model.
+		 * Dictionary<CandyType, List<Candy>> BagOfCandy { get; set; }
+		 */
+
+        public DatabaseContext(int tone) => Console.Beep(tone, 2500);
+
+        internal List<string> GetCandyTypes()
+        {
+            return Enum
+                .GetNames(typeof(CandyType))
+                .Select(candyType =>
+                    candyType.Humanize(LetterCasing.Title))
+                .ToList();
+        }
+
+        internal void SaveNewCandy(char selectedCandyMenuOption)
+        {
+            var candyOption = int.Parse(selectedCandyMenuOption.ToString());
+
+            var maybeCandyMaybeNot = (CandyType)selectedCandyMenuOption;
+            var forRealTheCandyThisTime = (CandyType)candyOption;
+
+            switch (forRealTheCandyThisTime)
+            {
+                case CandyType.TaffyNotLaffy:
+                    ++_countOfTaffy;
+                    break;
+                case CandyType.CandyCoated:
+                    ++_countOfCandyCoated;
+                    break;
+                case CandyType.CompressedSugar:
+                    ++_countOfChocolateBar;
+                    break;
+                case CandyType.ZagnutStyle:
+                    ++_countOfZagnut;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
